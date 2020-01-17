68739df12b015df4e8f3e258a8a48334b26171f7
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2020-01-17 17:22:41 +0000

    Writing to DynamoDB successfully

--- api/blog.js
+++ api/blog.js
@@ -1,8 +1,30 @@
+var AWS = require('aws-sdk');
+
 const createPost = (someText) => {
     var myObject = {
-        ID: 474,
-        BlogText: "Hello my name is Mike!"
+        ID: {N: '475'},
+        BlogText: {S:"Hello my name is Mike!"},
+        AnotherTestString: {S: "Hello again!"},
+        FinalEntry: {N: '001'}
     };
+
+    AWS.config.update({region: 'eu-west-1'});
+
+    var ddb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+    var params = {
+        TableName: 'MikeTest',
+        Item: myObject
+    };
+
+    ddb.putItem(params, function(error, data) {
+        if(error) {
+            console.log("Error: " + error);
+        }
+        else {
+            console.log("Success: " + data);
+        }
+    });
 
     console.log("ready to send to dynamodb!");
 }
