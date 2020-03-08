c199943233a68db41b517eb136dc82402cdaca6f
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2020-03-08 17:03:01 +0000

    Serverless deployment with create blog post

--- server.js
+++ server.js
@@ -1,32 +1,43 @@
-var express = require('express');
-var app = express();
-var port = 2300;
+// var express = require('express');
+// var app = express();
+// var port = 2300;
 
 var blog = require('./api/blog');
 
-app.post('/api/v1/create-blog', function (request, response) {
-    blog.createBlogPost(request)
-    response.status(200).send({
-        success: 'true'
-    });
-});
+// app.post('/api/v1/create-blog', function (request, response) {
+//     blog.createBlogPost(request)
+//     response.status(200).send({
+//         success: 'true'
+//     });
+// });
 
-app.get('/api/v1/get-blog', function(request, response) {
-    blog.getBlogPost(parseInt(request.query.id))
-    .then((data) => {
-        response.status(200).send({
-            success: 'true',
-            response: data
-        });
-    })
-    .catch((error) => {
-        console.log(error);
-        response.status(400).send({
-            success: false,
-            response: error.toString()
-        });
-    });
-});
+exports.createblog = async (event) => {
+    blog.createBlogPost(event)
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({success: true}),
+    }
 
-app.listen(port);
-console.log(`Server started running on ${port}`);
+    return response;
+};
+
+
+// app.get('/api/v1/get-blog', function(request, response) {
+//     blog.getBlogPost(parseInt(request.query.id))
+//     .then((data) => {
+//         response.status(200).send({
+//             success: 'true',
+//             response: data
+//         });
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//         response.status(400).send({
+//             success: false,
+//             response: error.toString()
+//         });
+//     });
+// });
+
+// app.listen(port);
+// console.log(`Server started running on ${port}`);
