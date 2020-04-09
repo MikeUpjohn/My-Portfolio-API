bba5df2d5dc0f82d2dbacd70cca72686f2077677
Mike Upjohn <mike.upjohn@cmap-software.com>
2020-04-09 22:19:52 +0100

    Dynamic data for creating blogs

--- api/blog.js
+++ api/blog.js
@@ -4,7 +4,9 @@ const documentClient = new AWS.DynamoDB.DocumentClient();
 const tableName = "Blog";
 
 exports.createBlog = async (event) => {
-    return createBlogPost(event).then(
+    var request = JSON.parse(event.body);
+    
+    return createBlogPost(request).then(
         data => {
             const response = {
                 statusCode: 200,
@@ -21,9 +23,7 @@ exports.createBlog = async (event) => {
         
             return response;
         }
-    )
-
-    
+    )    
 };
 
 exports.getBlog = async (event) => {
@@ -47,13 +47,13 @@ exports.getBlog = async (event) => {
     );
 };
 
-const createBlogPost = () => {
+const createBlogPost = (request) => {
     const blogData = {
         ID: Date.now(),
-        Title: 'Monday 20th May 2019 - HIGH Risk - TX/OK Panhandles',
-        BlogText: "<p>Awoke to a HIGH Risk across Eastern and Southern TX Panhandle, Western and Central Oklahoma. SPC highlighted risk areas in the HIGH Risk as OKC Metro, Norman, Lawton, Edmond and Midwest City. Tornado risk was 30% hatched first thing that morning, with OKC Metro, Norman, Lawton, Edmond, Midwest City all in the 30% hatched areas, and a large 15% area containing Tulsa, OK, Abilene, TX, Wichita Falls, TX, Broken Arrow, OK and Enid, OK. Hail risk was 45% hatched across much of the same area, along with a very similar 45% hatched area for damaging winds.</p>[[DSC__0741]]",
-        Tags : ['Tornado', 'Storm Chase', 'Storm Chasing', 'Severe Weather', 'Severe WX'],
-        GalleryItems: ['DSC__0741.jpg','DSC__0742.jpg']
+        Title: request.title,
+        BlogText: request.blogText,
+        Tags : request.tags,
+        GalleryItems: request.galleryItems
     };
 
     var params = {
